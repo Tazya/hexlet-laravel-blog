fabd21daa34c151ed08b8f8a69d145f51005dd61
Pavel Chernyshev <vengador32@gmail.com>
2020-05-31 20:44:41 +0600

    add resource routes

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreArticle;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $articles = Article::paginate(2);
@@ -16,18 +21,23 @@ class ArticleController extends Controller
         return view('article.index', compact('articles', 'flashMessage'));
     }
 
-    public function show($id)
-    {
-        $article = Article::findOrFail($id);
-        return view('article.show', compact('article'));
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         $article = new Article();
         return view('article.create', compact('article'));
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(StoreArticle $request)
     {
         $data = $request->validated();
@@ -41,15 +51,37 @@ class ArticleController extends Controller
             ->route('articles.index');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
     {
-        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
         return view('article.edit', compact('article'));
     }
 
-    public function update(StoreArticle $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreArticle $request, Article $article)
     {
-        $article = Article::findOrFail($id);
         $data = $request->validated();
 
         $article->fill($data);
@@ -60,9 +92,14 @@ class ArticleController extends Controller
             ->route('articles.index');
     }
 
-    public function destroy($id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
     {
-        $article = Article::find($id);
         if ($article) {
             $article->delete();
         }
--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,4 @@ Route::get('/', function () {
 
 Route::get('/about', 'PageController@about');
 
-Route::get('/articles', 'ArticleController@index')
-    ->name('articles.index');
-Route::get('/articles/create', 'ArticleController@create')
-    ->name('articles.create');
-Route::post('/articles', 'ArticleController@store')
-    ->name('articles.store');
-Route::get('/articles/{id}/edit', 'ArticleController@edit')
-    ->name('articles.edit');
-Route::get('/articles/{id}', 'ArticleController@show')
-    ->name('articles.show');
-Route::patch('/articles/{id}', 'ArticleController@update')
-    ->name('articles.update');
-Route::delete('/articles/{id}', 'ArticleController@destroy')
-    ->name('articles.destroy');
+Route::resource('/articles', 'ArticleController');
